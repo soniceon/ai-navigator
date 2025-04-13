9faf3ea639a94d740b0661b554131526d8cb68b0
soniceon <58773650+soniceon@users.noreply.github.com>
2025-04-13 10:37:14 +0800

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Wait for DOM to fully load
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Website loaded');
+    
+    // Example: Display current time in console
+    console.log('Current time:', new Date().toLocaleString());
+    
+    // More functionality will be added here
+});
